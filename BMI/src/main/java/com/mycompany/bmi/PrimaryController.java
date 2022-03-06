4d507397286f8df4534fc25c4d3912f8ac57fe0a
IrinaLuminesk <1951012084nhan@ou.edu.vn>
2022-03-06 12:16:39 +0700

    init project

---
+++ BMI/src/main/java/com/mycompany/bmi/PrimaryController.java
@@ -0,0 +1,28 @@
+package com.mycompany.bmi;
+
+import java.text.DecimalFormat;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class PrimaryController
+{
+    @FXML private TextField Weight;
+    @FXML private TextField Height;
+    @FXML private Label KQ;
+    public void Handler(ActionEvent event)
+    {
+        Double BMI = Double.parseDouble(this.Weight.getText()) / Math.pow(Double.parseDouble(this.Height.getText()) / 100, 2);
+        DecimalFormat f = new DecimalFormat("#.##");
+        String kq = f.format(BMI);
+        if(BMI < 18.5)
+            kq += " - Cân nặng thấp (Gầy)"; 
+        if(BMI >= 18.5 && BMI <= 24.5)
+            kq += " - Bình thường";
+        if(BMI > 24.5)
+            kq += " - Béo phì";
+        KQ.setText(kq);
+    }
+}
